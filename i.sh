f595297eb65e383c215e6028f5303bd9e1d0b685
pfzim <pfzim@users.noreply.github.com>
2014-09-02 15:25:24 +0400

    no changes

--- i.sh
+++ i.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Boot XBMCbuntu, CTRL+F1, login and run:
+# Boot XBMCbuntu, CTRL+ALT+F1, login and run:
 # wget https://raw.githubusercontent.com/pfzim/xbmc-config/master/i.sh
 # chmod a+rx i.sh
 # sudo ./i.sh
