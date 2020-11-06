33bbef7c7a2b05979ab70ded5353bdada2eb995c
Dmitry V. Zimin <pfzim@mail.ru>
2020-11-06 20:01:30 +0300

    Reload config instead restart motion

--- motion/on_cam_disable.sh
+++ motion/on_cam_disable.sh
@@ -3,6 +3,13 @@
 [ -f /etc/motion/conf.d/camera$1.conf ] || exit 1
 
 rm -f /etc/motion/conf.d/camera$1.conf
-systemctl restart motion
+
+#systemctl restart motion
+
+pid=`pidof -s motion`
+if [ $? -eq 0 ] ; then
+  kill -s 1 $pid
+  exit 0
+fi
 
 exit 0
--- motion/on_cam_enable.sh
+++ motion/on_cam_enable.sh
@@ -3,6 +3,13 @@
 [ -f /etc/motion/conf.avail/camera$1.conf ] || exit 1
 
 ln -s /etc/motion/conf.avail/camera$1.conf /etc/motion/conf.d/
-systemctl restart motion
 
-exit 0
+#systemctl restart motion
+
+pid=`pidof -s motion`
+if [ $? -eq 0 ] ; then
+  kill -s 1 $pid
+  exit 0
+fi
+
+exit 1
